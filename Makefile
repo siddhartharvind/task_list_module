143b4a27f8c5a9b8090d320d407503a6b2a36571
siddhartharvind <sidarvind11@gmail.com>
2023-04-16 21:04:53 +0530

    Added colour coding of process states

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 obj-m += task_list_module.o
+ifeq ($(colors),1)
+	NOESC := --noescape
+	CFLAGS_${obj-m} := -DPRINTK_COLORS
+endif	
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- task_list_module.c
+++ task_list_module.c
@@ -6,32 +6,59 @@
 #include <linux/sched/signal.h>
 
 
+// Color codes for printk()
+#ifdef PRINTK_COLORS
+
+#define RED(s) 		"\e[01;31m" s "\e[0m"
+#define GRN(s) 		"\e[01;32m" s "\e[0m"
+#define YEL(s) 		"\e[01;33m" s "\e[0m"
+#define MAG(s) 		"\e[01;35m" s "\e[0m"
+#define CYN(s) 		"\e[01;36m" s "\e[0m"
+#define WHT(s) 		"\e[01;37m" s "\e[0m"
+#define REDBG(s) 	"\e[01;41m" s "\e[0m"
+
+#else // PRINTK_COLORS
+
+#define RED(s) s
+#define GRN(s) s
+#define YEL(s) s
+#define MAG(s) s
+#define CYN(s) s
+#define WHT(s) s
+#define REDBG(s) s
+
+#endif // PRINTK_COLORS
+
+
 // This function takes a task's state and returns
 // its meaning using the macros defined in
 // <linux/sched.h>
 static char *get_task_state(unsigned int state, 
 	unsigned int exit_state)
 {
-	if (!state) 						return "Running (R)";
-	if (state & TASK_INTERRUPTIBLE) 	return "Sleeping (S)";
-	if (state & TASK_UNINTERRUPTIBLE) 	return "Disk Sleep (D)";
-	if (state & __TASK_STOPPED) 		return "Stopped (T)";
-	if (exit_state & EXIT_ZOMBIE) 		return "Zombie (Z)";
-	return "Unknown";
+	if (!state) 						return GRN("Running (R)");
+	if (state & TASK_INTERRUPTIBLE) 	return CYN("Sleeping (S)");
+	if (state & TASK_UNINTERRUPTIBLE) 	return YEL("Disk Sleep (D)");
+	if (state & __TASK_STOPPED) 		return RED("Stopped (T)");
+	if (exit_state & EXIT_ZOMBIE) 		return REDBG("Zombie (Z)");
+	return MAG("Unknown");
 }
 
 // This function iterates through the task list
 // and prints each task's PID, name and state.
 static void print_procs(void)
 {
+	struct task_struct *task;
+	char *fmt;
+
 	printk(KERN_INFO "%s: [ %5s ]\t[ %15s ]\t[ %14s ]\n", \
 		THIS_MODULE->name, "PID", "NAME", "STATE");
-	struct task_struct *task;
+
+	fmt = KERN_INFO "%s: [ %5d ]\t " WHT("%16s") "\t %15s\n";
 	for_each_process(task) {
-		printk(KERN_INFO "%s: [ %5d ]\t %16s\t %15s\n", \
-			THIS_MODULE->name, task->pid, task->comm, \
-			get_task_state(task->__state, task->exit_state)
-		);
+		printk(fmt, THIS_MODULE->name, task->pid, \
+			task->comm, get_task_state(task->__state, \
+			task->exit_state));
 	}
 }
 
