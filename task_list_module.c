56555fb338f49b1ca41ec56d54c0cb84325cc1d2
Siddharth Arvind <sidarvind11@gmail.com>
2023-04-24 20:04:53 +0530

    Format code to use spaces and make it more readable

--- task_list_module.c
+++ task_list_module.c
@@ -1,18 +1,19 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
 
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("This is a simple Linux kernel module \
-	that lists all the current tasks in a Linux system, \
-	starting from the `init` task.");
+MODULE_DESCRIPTION(
+    "This is a simple Linux kernel module "
+    "that lists all the current tasks in a Linux system, "
+    "starting from the `init` process."
+);
 MODULE_AUTHOR("Siddharth Arvind");
-
 #define MODULE_NAME THIS_MODULE->name
+
 
 // Color codes for printk()
 #define PRINTK_COLORS
@@ -21,104 +22,121 @@ MODULE_AUTHOR("Siddharth Arvind");
 
 // This function takes a task's state and returns
 // its meaning using the macros defined in <linux/sched.h>
-static char *get_task_state(unsigned int state, 
-	unsigned int exit_state)
+static
+char *get_task_state_name(unsigned int state, unsigned int exit_state)
 {
-	if (!state) 						return GRN("Running (R)");
-	if (state & TASK_INTERRUPTIBLE) 	return CYN("Sleeping (S)");
-	if (state & TASK_UNINTERRUPTIBLE) 	return YEL("Disk Sleep (D)");
-	if (state & __TASK_STOPPED) 		return RED("Stopped (T)");
-	if (exit_state & EXIT_ZOMBIE) 		return REDBG("Zombie (Z)");
-	return MAG("Unknown");
+    static const unsigned int task_running = 0;
+
+    if (     state == task_running        )  return   GRN( "Running (R)"    );
+    if (     state  & TASK_INTERRUPTIBLE  )  return   CYN( "Sleeping (S)"   );
+    if (     state  & TASK_UNINTERRUPTIBLE)  return   YEL( "Disk Sleep (D)" );
+    if (     state  & __TASK_STOPPED      )  return   RED( "Stopped (T)"    );
+    if (exit_state  & EXIT_ZOMBIE         )  return REDBG( "Zombie (Z)"     );
+
+    return MAG("Unknown");
 }
 
 
 
 // This function prints out the header row for the task
 // details to be printed below it.
-static void print_task_header_row(void)
+static
+void print_task_header_row(void)
 {
-	static const char *const fmt = \
-		KERN_INFO "%s: " BLUBG("%s") "\t    " BLUBG("%s") \
-			"\t\t   " BLUBG("%s") "\n";
+    static const char *const fmt = KERN_INFO    \
+        "%s: " BLUBG("%s") "\t    " BLUBG("%s") \
+        "\t\t   " BLUBG("%s") "\n";
 
-	printk(fmt, MODULE_NAME, "[  PID  ]", "[ NAME ]", "[ STATE ]");
+    printk(fmt, MODULE_NAME,
+        "[  PID  ]",
+        "[ NAME ]",
+        "[ STATE ]"
+    );
 }
 
 
 // This function prints out the details of a given task:
 // PID, name, and state.
-static void print_task_details(struct task_struct *task)
+static
+void print_task_details(struct task_struct *task)
 {
-	static const char *const fmt = \
-		KERN_INFO "%s: [ %5d ]" WHT("%21s") "\t\t%15s\n";
+    static const char *const fmt = KERN_INFO \
+        "%s: [ %5d ]" WHT("%21s") "\t\t%15s\n";
 
-	printk(fmt, MODULE_NAME, task->pid,
-		task->comm, get_task_state(task->__state,
-		task->exit_state));
+    printk(fmt, MODULE_NAME,
+        task->pid,
+        task->comm,
+        get_task_state_name(task->__state, task->exit_state)
+    );
 }
 
 
 // This function linearly iterates through the task list
 // and prints each task's PID, name and state.
-static void print_processes_linear(void)
+static
+void print_processes_linear(void)
 {
-	print_task_header_row();
+    print_task_header_row();
 
-	struct task_struct *task;
-	for_each_process(task) {
-		print_task_details(task);
-	}
+    struct task_struct *task;
+    for_each_process(task) {
+        print_task_details(task);
+    }
 }
 
 
 
 // This is a recursive helper function that iterates through
 // the child tasks of a given task using depth-first search (DFS).
-static void print_processes_dfs_helper(struct task_struct *task)
+static
+void print_processes_dfs_helper(struct task_struct *task)
 {
-	print_task_details(task);
+    print_task_details(task);
 
-	// DFS on current task's children
-	struct task_struct *child_task;
-	struct list_head   *list;
+    // DFS on current task's children
+    struct task_struct *child_task;
+    struct list_head   *list;
 
-	list_for_each(list, &task->children) {
-		child_task = list_entry(list, struct task_struct, sibling);
-		print_processes_dfs_helper(child_task);
-	}
+    list_for_each(list, &task->children) {
+        child_task = list_entry(list, struct task_struct, sibling);
+        print_processes_dfs_helper(child_task);
+    }
 }
+
 
 // This function iterates through the task list using depth-first search
 // (DFS) and prints each task's PID, name and state.
-static void print_processes_dfs(void)
+static
+void print_processes_dfs(void)
 {
-	print_task_header_row();
+    print_task_header_row();
 
-	// Print process tree
-	print_processes_dfs_helper(&init_task);
+    // Print process tree
+    print_processes_dfs_helper(&init_task);
 }
 
 
 
 // This function is called when the module is loaded.
+static
 int task_list_init(void)
 {
-	printk(KERN_INFO "%s: Loading module...\n\n", \
-		MODULE_NAME);
+    printk(KERN_INFO "%s: Loading module...\n\n", \
+        MODULE_NAME);
 
-	print_processes_dfs();
-	printk("\n");
+    print_processes_linear();
+    printk("\n");
 
-	return 0;
+    return 0;
 }
 
 
 // This function is called when the module is removed.
+static
 void task_list_exit(void)
 {
-	printk(KERN_INFO "%s: Removing module...\n\n", \
-		MODULE_NAME);
+    printk(KERN_INFO "%s: Removing module...\n\n", \
+        MODULE_NAME);
 }
 
 
